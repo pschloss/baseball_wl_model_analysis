8353c60fe4da1cc0112d799484cfd81c9b4eb451
Pat Schloss <pschloss@umich.edu>
2018-07-17 08:49:52 -0400

    Ascertain the favorite for each game

--- analysis.R
+++ analysis.R
@@ -17,11 +17,17 @@ current_date <- now()
 game_data <- read_csv(file="https://projects.fivethirtyeight.com/mlb-api/mlb_elo.csv",
 											col_types=cols(date=col_date(),
 																		 season=col_integer(),
-																		 rating1_post=col_double(),
-																		 rating2_post=col_double(),
+																		 rating_prob1=col_double(),
+																		 rating_prob2=col_double(),
 																		 score1=col_integer(),
 																		 score2=col_integer())
 											) %>%
 						filter(date < current_date)
+
+
+favorite_win_prob <- game_data %>%
+	mutate(fav_538_won=ifelse(rating_prob1>rating_prob2, score1 > score2, score2 > score1),
+				 fav_538_prob=ifelse(rating_prob1>rating_prob2, rating_prob1, rating_prob2)) %>%
+	select(season, date, team1, team2, fav_538_won, fav_538_prob)
 
 
