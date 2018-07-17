3e6ffa5d19873d27823ec7dd542be1ba40dd8f39
Pat Schloss <pschloss@umich.edu>
2018-07-17 14:06:38 -0400

    Plot model probabilities

--- analysis.R
+++ analysis.R
@@ -12,6 +12,20 @@
 library(tidyverse)
 library(lubridate)
 library(broom)
+library("wesanderson")
+
+
+get_wp <- function(A, B){
+	
+	if((A == 0 && B == 0) || (A == 1 && B == 1)){
+		0.5
+	} else {
+		A * (1-B) / (A*(1-B) + B*(1-A))
+	}
+	
+}
+
+
 
 current_date <- now()
 
@@ -32,64 +46,122 @@ game_data <- read_csv(file="https://projects.fivethirtyeight.com/mlb-api/mlb_elo
 favorite_win_prob <- game_data %>%
 	mutate(fav_538_won=ifelse(rating_prob1>rating_prob2, score1 > score2, score2 > score1),
 				 fav_538_prob=ifelse(rating_prob1>rating_prob2, rating_prob1, rating_prob2)) %>%
-	select(season, date, team1, team2, fav_538_won, fav_538_prob)
+	select(season, date, team1, team2, score1, score2, fav_538_won, fav_538_prob)
 
 
-overall_win_prob <- mean(favorite_win_prob$fav_538_won)
+win_losses_live <- favorite_win_prob %>%
+	mutate(win1=score1>score2,
+				 win2=score2>score1,
+				 team_win1 = paste(team1, win1, sep="_"),
+				 team_win2 = paste(team2, win2, sep="_")) %>%
+	gather(one_two, team_win, team_win1, team_win2) %>%
+	separate(team_win, into=c("team", "win"), sep="_", convert=TRUE) %>%
+	arrange(date) %>%
+	group_by(season, team) %>%
+	mutate(wins=c(0, na.omit(lag(cumsum(win)))),
+				 losses=c(0, na.omit(lag(cumsum(!win)))),
+				 avg=c(0, na.omit(wins/(wins+losses)))
+				 )  %>% 
+	ungroup() %>%
+	select(season, team, date, avg)
+	
+
+win_losses_season <- favorite_win_prob %>%
+	mutate(win1=score1>score2,
+				 win2=score2>score1,
+				 team_win1 = paste(team1, win1, sep="_"),
+				 team_win2 = paste(team2, win2, sep="_")) %>%
+	gather(one_two, team_win, team_win1, team_win2) %>%
+	separate(team_win, into=c("team", "win"), sep="_", convert=TRUE) %>%
+	group_by(team, season) %>%
+	summarize(current_avg = mean(win)) %>%
+	mutate(prev_avg = c(0.5, na.omit(lag(current_avg)))) %>%
+	ungroup()
+
+
+
+# join in win_losses_live into favorite_win_prob
+favorite_win_prob <- favorite_win_prob %>%
+	inner_join(., win_losses_live, by=c("team1"="team", "season", "date")) %>%
+	inner_join(., win_losses_live, by=c("team2"="team", "season", "date")) %>%
+	mutate(win_prob1=map2_dbl(avg.x, avg.y, get_wp),
+				 win_prob2=1-win_prob1,
+				 fav_wplive_won=ifelse(win_prob1 > win_prob2, score1 > score2, score2 > score1),
+				 fav_wplive_prob=ifelse(win_prob1 > win_prob2, win_prob1, win_prob2)) %>%
+	select(season, date, team1, team2, score1, score2, fav_538_won, fav_538_prob, fav_wplive_won, fav_wplive_prob)
+	
+
+# join in win_losses_season into favorite_win_prob
+favorite_win_prob <- favorite_win_prob %>%
+	inner_join(., win_losses_season, by=c("team1"="team", "season")) %>%
+	inner_join(., win_losses_season, by=c("team2"="team", "season")) %>%
+	mutate(win_prob1=map2_dbl(current_avg.x, current_avg.y, get_wp),
+				 win_prob2=1-win_prob1,
+				 fav_wpcurrent_won=ifelse(win_prob1 > win_prob2, score1 > score2, score2 > score1),
+				 fav_wpcurrent_prob=ifelse(win_prob1 > win_prob2, win_prob1, win_prob2),
+				 
+				 win_prob1=map2_dbl(prev_avg.x, prev_avg.y, get_wp),
+				 win_prob2=1-win_prob1,
+				 fav_wpprev_won=ifelse(win_prob1 > win_prob2, score1 > score2, score2 > score1),
+				 fav_wpprev_prob=ifelse(win_prob1 > win_prob2, win_prob1, win_prob2)
+				 ) %>%
+	select(season, date, team1, team2, score1, score2, fav_538_won, fav_538_prob, fav_wplive_won, fav_wplive_prob, fav_wpcurrent_won, fav_wpcurrent_prob, fav_wpprev_won, fav_wpprev_prob)
+
+
+# make the data frame "tidy"
+tidy_win_prob <- favorite_win_prob %>% 
+	mutate(fte=paste(fav_538_won, fav_538_prob, sep="_"),
+				 wplive=paste(fav_wplive_won, fav_wplive_prob, sep="_"),
+				 wpcurrent=paste(fav_wpcurrent_won, fav_wpcurrent_prob, sep="_"),
+				 wpprev=paste(fav_wpprev_won, fav_wpprev_prob, sep="_")) %>%
+	select(-starts_with("fav")) %>%
+	gather(model, won_prob, fte, wplive, wpcurrent, wpprev) %>%
+	separate(won_prob, into=c("won", "prob"), sep="_", convert=TRUE)
+
+
+
+overall_win_prob <- tidy_win_prob %>%
+	group_by(model) %>%
+	summarize(mean=mean(won))
+
 
 #Plot the fraction games that the favorite has won over the history of baseball
-favorite_win_prob %>%
-	group_by(season) %>%
-	summarize(fraction_favorite_won = mean(fav_538_won)) %>%
-	ggplot(aes(x=season, y=fraction_favorite_won)) +
-	geom_hline(aes(yintercept=overall_win_prob), color="lightgray") +
+tidy_win_prob %>%
+	group_by(season, model) %>%
+	summarize(fraction_favorite_won = mean(won)) %>%
+	ungroup() %>%
+	ggplot(aes(x=season, y=fraction_favorite_won, group=model, color=model)) +
+	geom_hline(data=overall_win_prob, aes(yintercept=mean, group=model, color=model)) +
 	geom_line() + 
 	theme_classic() +
 	coord_cartesian(ylim=c(0,1)) +
 	labs(x="Season", y="Fraction of games favorite won",
-			 title="The 538 model does a better than average job of predicting the winner of baseball games",
-			 subtitle=paste0("Since 1871, the favorite has won ",  round(100*overall_win_prob, digits=1),"% of their games"))
+			 title="The Winning Percentage model can out perform the 538 ELO model if it uses end of\nseason winning averages") +
+	scale_color_manual(name=NULL,
+										 breaks=c("fte", "wpcurrent", "wplive", "wpprev"),
+										 labels=c("538", "WP Curent", "WP Live", "WP Previous"),
+										 values=wes_palette("Darjeeling2"))
 
 
 
 # Plot the observed versus expected fraction of games won by the favorite
-all_predicted_observed <- favorite_win_prob %>% 
-	mutate(fav_538_prob = round(fav_538_prob, digits=2)) %>%
-	group_by(fav_538_prob) %>%
+tidy_win_prob %>% 
+	mutate(prob = round(prob, digits=2)) %>%
+	group_by(prob, model) %>%
 	summarize(games = n(),
-						wins = sum(fav_538_won),
-						observed = wins / games)
-
-binomial_fit_validation <- all_predicted_observed %>%
-	mutate(prob = fav_538_prob) %>%
-	group_by(fav_538_prob) %>%
-	nest() %>%
-	mutate(binomial = map(data, function(df)
-																	tidy(binom.test(x=as.integer(df$games * df$prob), 
-																									n=df$games),
-																			 						p=df$prob
-																									)
-																			 )
-												) %>%
-	unnest() %>%
-	select(fav_538_prob, games, wins, observed, conf.low, conf.high)
-
-
-
-binomial_fit_validation %>%
-	ggplot(aes(x=fav_538_prob, y=observed)) +
-	geom_ribbon(aes(ymin=conf.low, ymax=conf.high), fill="lightgray") +
-	geom_abline(aes(intercept=0, slope=1), color="darkgray") +
-	geom_point() +
-	theme_classic() + 
-	coord_cartesian(ylim=c(0,1)) +
-	labs(x="Predicted Probability of Winning",
-			 y="Observed Probability of Winning",
-			 title="The 538 model underpredicts the true ability of the favorite to win",
-			 subtitle="All games from 1871 to present")
-
-
-
+						wins = sum(won),
+						observed = wins / games) %>%
+	ggplot(aes(x=prob,  y=observed, group=model, color=model)) +
+		geom_abline(aes(intercept=0, slope=1), color="gray") +
+		geom_line() +
+		theme_classic() + 
+		scale_color_manual(name=NULL,
+										 breaks=c("fte", "wpcurrent", "wplive", "wpprev"),
+										 labels=c("538", "WP Curent", "WP Live", "WP Previous"),
+										 values=wes_palette("Darjeeling2")) +
+		labs(x="Predicted Win Probability", y="Observed Win Probability",
+				 title="The 538 and WP Current models generate more reliable win probabilities than the\nWP Live or Previous models",
+				 subtitle="All data since 1871")
 
 
 
