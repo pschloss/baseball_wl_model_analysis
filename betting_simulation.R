e97a643a9972e63302bbcc41a1cb941e4e32de7e
Pat Schloss <pschloss@umich.edu>
2018-07-20 13:02:07 -0400

    Initial commit

---
+++ betting_simulation.R
@@ -0,0 +1,17 @@
+#***********************************************************************************
+#
+# file: betting_simulation.R
+# author: Pat Schloss
+# date: July 20, 2018
+#
+# Purpose: Simulate how much money we'd make or lose based on betting $100 on each
+# of the favorites from our models
+#
+#***********************************************************************************
+
+library(tidyverse)
+library(lubridate)
+library(broom)
+library("wesanderson")
+
+
